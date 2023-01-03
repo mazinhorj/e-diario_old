8bf7f7b053dd65e3339cc2a1d71d93bc1458b430
mazinhorj <adm.osmarsilva@gmail.com>
2023-01-03 16:55:27 -0300

    mais

---
+++ rascunhos/database.js
@@ -0,0 +1,39 @@
+db.distrito.insert({
+  distritos_numero: [
+    {
+      distrito_numero: "1",
+    },
+    {
+      distrito_numero: "2",
+    },
+    {
+      distrito_numero: "3",
+    },
+    {
+      distrito_numero: "4",
+    },
+  ],
+});
+
+
+const data = [
+  {
+    janeiro: [
+      {
+        dia: 1,
+        letivo: 1, //boolean 1- letivo / 0- não letivo
+      },
+      {
+        dia: 2,
+        letivo: 1, //boolean 1- letivo 0- não letivo
+      },
+      {
+        dia: 3,
+        letivo: 1, //boolean 1- letivo 0- não letivo
+      },
+    ]
+  }
+];
+
+export default data;
+
